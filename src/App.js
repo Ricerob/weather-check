a25aecc3da6344e13c9a04979e783ed0b8f7e1b6
Robby <riceslif@gmail.com>
2022-01-06 17:27:25 -0600

    Alpha release

--- src/App.js
+++ src/App.js
@@ -1,28 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
+import ReactDOM from 'react-dom';
 
 function App() {
   const [location, setLocation] = useState("Chicago");
 
-  const handleSubmit = (event) => {
+  async function handleSubmit(event){
     event.preventDefault();
-    //Get weather data based on user input
-    alert(location);
+
+    let api_key = process.env.REACT_APP_WEATHER_API_KEY;
+    let url = "http://api.weatherstack.com/current?access_key=" + api_key + "&query=" + location + "&units=f";
+    console.log(url);
+    let response = await fetch(url)
+        .catch(error => {
+            console.log(error);
+        });
+    let data = await response.json();
+
+    console.log(data);
+
+    document.getElementById("submitForm").style.visibility= "hidden";
+    document.getElementById("welcome").style.visibility= "hidden";
+    /*ReactDOM.render(<button id="reset" onClick={window.location.reload()}>
+      New Request
+    </button>, document.getElementById("submitForm"));*/
+
+    let insert = <div>
+      <h3>{data.location.name}</h3>
+      <h4>{data.location.country}</h4>
+      <img src={data.current.weather_icons[0]} alt="Weather logo"></img>
+      <p>{data.current.temperature}° and {(data.current.weather_descriptions[0]).toLowerCase()}</p>
+      <p>Feels like {data.current.feelslike}°</p>
+      <p>Wind speed at {data.current.wind_speed} mph</p>
+    </div>
+
+    ReactDOM.render(insert, document.getElementById("insert"));
   }
 
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} id="submitForm">
         <label>Enter location:
           <input
             type="text"
+            placeholder="Chicago"
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
         <input type="submit"/>
       </form>
-      <h3>{location}</h3>
+      <div id="insert">
+      </div>
+      <h1 id="welcome">Welcome to Weather Check! Type in a city above to see their current weather.</h1>
     </div>
   );
 }
